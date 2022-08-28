8d18428a5bc838c5738a8dc6d3db51d53c954c3d
Zeerocious <zoro_z_99@hotmail.com>
2022-08-28 10:53:21 -0700

    proped up the destination so the data is saved in the parent shipping

--- src/Destination.js
+++ src/Destination.js
@@ -1,19 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
-import SimpleBar from "simplebar-react";
 import { Button, Box, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 
-export default function Destination() {
-  const [contact, setContact] = useState({
-    firstName: "",
-    lastName: "",
-    address: "",
-    address2: "",
-    idNumber: "",
-    email: "",
-    phoneNumber: "",
-  });
-  const [contacts, setContacts] = useState([]);
+export default function Destination(props) {
+  const {
+    city,
+    setCity,
+    state,
+    setState,
+    contact,
+    contacts,
+    setContact,
+    setContacts,
+  } = props;
 
   const addContact = (e) => {
     e.preventDefault();
@@ -57,155 +56,177 @@ export default function Destination() {
     setContacts((contacts) => contacts.filter((_, i) => i !== index));
   };
   return (
-    
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+        mt: 5,
+        width: "100%",
+      }}
+    >
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          height: "100%",
-          mt: 5,
-          width: "100%",
+          maxWidth: 500,
+          p: 2,
         }}
       >
+        <Typography sx={{ m: 1 }} variant="h5">
+          DESTINATION
+        </Typography>
+
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            maxWidth: 500,
-            p: 2,
           }}
         >
-          <Typography sx={{ m: 1 }} variant="h5">
-            DESTINATION
+          <TextField
+            sx={{ m: 1 }}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            required
+            label="City"
+            defaultValue={city}
+          />
+          <TextField
+            sx={{ m: 1 }}
+            onChange={(e) => {
+              setState(e.target.value);
+            }}
+            required
+            label="State"
+            defaultValue={state}
+          />
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Typography sx={{ m: 1 }} variant="h7">
+            Add Receiver
           </Typography>
           <Box
             sx={{
               display: "flex",
             }}
           >
-            <TextField sx={{ m: 1 }} required label="City" />
-            <TextField sx={{ m: 1 }} required label="State" />
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <Typography sx={{ m: 1 }} variant="h7">
-              Add Receiver
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
+            <TextField
+              onChange={(e) => {
+                setContact((contact) => ({
+                  ...contact,
+                  firstName: e.target.value,
+                }));
               }}
-            >
-              <TextField
-                onChange={(e) => {
-                  console.log(e);
-                  setContact((contact) => ({
-                    ...contact,
-                    firstName: e.target.value,
-                  }));
-                }}
-                sx={{ m: 1 }}
-                required
-                label="First Name"
-              />
-              <TextField
-                onChange={(e) =>
-                  setContact((contact) => ({
-                    ...contact,
-                    lastName: e.target.value,
-                  }))
-                }
-                sx={{ m: 1 }}
-                required
-                label="Last Name"
-              />
-            </Box>
-            <TextField
-              onChange={(e) =>
-                setContact((contact) => ({
-                  ...contact,
-                  address: e.target.value,
-                }))
-              }
               sx={{ m: 1 }}
               required
-              label="Address 1"
+              label="First Name"
+              defaultValue={contact.firstName}
             />
             <TextField
               onChange={(e) =>
                 setContact((contact) => ({
                   ...contact,
-                  address2: e.target.value,
+                  lastName: e.target.value,
                 }))
               }
               sx={{ m: 1 }}
               required
-              label="Address 2 (Apt, Suite, Unit)"
+              label="Last Name"
+              defaultValue={contact.lastName}
             />
-            <TextField
-              onChange={(e) =>
-                setContact((contact) => ({
-                  ...contact,
-                  email: e.target.value,
-                }))
-              }
-              sx={{ m: 1 }}
-              required
-              label="Email"
-            />
-            <Box
-              sx={{
-                display: "flex",
-              }}
-            >
-              <TextField
-                onChange={(e) =>
-                  setContact((contact) => ({
-                    ...contact,
-                    idNumber: e.target.value,
-                  }))
-                }
-                sx={{ m: 1 }}
-                required
-                label="ID Number"
-              />
-              <TextField
-                onChange={(e) =>
-                  setContact((contact) => ({
-                    ...contact,
-                    phoneNumber: e.target.value,
-                  }))
-                }
-                sx={{ m: 1 }}
-                required
-                label="Phone Number"
-              />
-            </Box>
-            <Button onClick={addContact}>Add Receiver</Button>
           </Box>
-          <Typography sx={{ m: 1 }} variant="h7">
-            Receiver
-          </Typography>
-          <Box>
-            {contacts.map((contact, index) => {
-              return (
-                <Box key={contact.id}>
-                  <Typography>
-                    Name: {contact.firstName} {contact.lastName}
-                  </Typography>
-                  <Typography>Address 1: {contact.address}</Typography>
-                  <Typography>Address 2: {contact.address2}</Typography>
-                  <Typography>Email: {contact.email}</Typography>
-                  <Typography>ID Number: {contact.idNumber}</Typography>
-                  <Typography>Phone Number: {contact.phoneNumber}</Typography>
-                  <Button onClick={() => deleteContact(index)}>
-                    Delete Receiver
-                  </Button>
-                </Box>
-              );
-            })}
+          <TextField
+            onChange={(e) =>
+              setContact((contact) => ({
+                ...contact,
+                address: e.target.value,
+              }))
+            }
+            sx={{ m: 1 }}
+            required
+            label="Address 1"
+            defaultValue={contact.address}
+          />
+          <TextField
+            onChange={(e) =>
+              setContact((contact) => ({
+                ...contact,
+                address2: e.target.value,
+              }))
+            }
+            sx={{ m: 1 }}
+            label="Address 2 (Apt, Suite, Unit)"
+            defaultValue={contact.address2}
+          />
+          <TextField
+            onChange={(e) =>
+              setContact((contact) => ({
+                ...contact,
+                email: e.target.value,
+              }))
+            }
+            sx={{ m: 1 }}
+            required
+            label="Email"
+            defaultValue={contact.email}
+          />
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <TextField
+              onChange={(e) =>
+                setContact((contact) => ({
+                  ...contact,
+                  idNumber: e.target.value,
+                }))
+              }
+              sx={{ m: 1 }}
+              required
+              label="ID Number"
+              defaultValue={contact.idNumber}
+            />
+            <TextField
+              onChange={(e) =>
+                setContact((contact) => ({
+                  ...contact,
+                  phoneNumber: e.target.value,
+                }))
+              }
+              sx={{ m: 1 }}
+              required
+              label="Phone Number"
+              defaultValue={contact.phoneNumber}
+            />
           </Box>
+          <Button onClick={addContact}>Add Receiver</Button>
+        </Box>
+        <Typography sx={{ fontFamily: "roboto" }} variant="h5">
+          Receivers
+        </Typography>
+
+        <Box>
+          {contacts.map((contact, index) => {
+            return (
+              <Box key={contact.id}>
+                <Typography>
+                  Name: {contact.firstName} {contact.lastName}
+                </Typography>
+                <Typography>Address 1: {contact.address}</Typography>
+                <Typography>Address 2: {contact.address2}</Typography>
+                <Typography>Email: {contact.email}</Typography>
+                <Typography>ID Number: {contact.idNumber}</Typography>
+                <Typography>Phone Number: {contact.phoneNumber}</Typography>
+                <Button onClick={() => deleteContact(index)}>
+                  Delete Receiver
+                </Button>
+              </Box>
+            );
+          })}
         </Box>
       </Box>
+    </Box>
   );
 }
--- src/Shipping.js
+++ src/Shipping.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Box,
@@ -9,93 +9,91 @@ import {
 import SenderInfo from "./SenderInfo";
 import GoodsInfo from "./GoodsInfo";
 import Destination from "./Destination";
+import SimpleBar from "simplebar-react";
 
 const steps = ["Sender Info", "Destination", "Goods Info"];
 
+
 export default function Shipping() {
+  const [city, setCity] = useState()
+  const [state, setState] = useState()
+  const [contact, setContact] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    address2: "",
+    idNumber: "",
+    email: "",
+    phoneNumber: "",
+  });
+  const [contacts, setContacts] = useState([]);
+
   const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
+
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+  console.log(city, state)
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-      }}
-    >
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          return (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === 0 ? (
-        <>
-        <SenderInfo />
-        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </>
-      ) : activeStep === 1 ? (
-        <>
-        <Destination />
-        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </>
-      ) : (
-        <>
-        <GoodsInfo />
-        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </>
-      )}
-    </Box>
+    <SimpleBar style={{ height: "100%", minHeight: 0 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          m: 2,
+        }}
+      >
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === 0 ? (
+          <>
+            <SenderInfo />
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+                Back
+              </Button>
+ 
+              <Button onClick={handleNext}>{activeStep === steps.length - 1 ? "Finish" : "Next"}</Button>
+            </Box>
+          </>
+        ) : activeStep === 1 ? (
+          <>
+            <Destination city={city} setCity={setCity} state={state} setState={setState} contact={contact} contacts={contacts} setContact={setContact} setContacts={setContacts} />
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+                Back
+              </Button>
+ 
+              <Button onClick={handleNext}>{activeStep === steps.length - 1 ? "Finish" : "Next"}</Button>
+            </Box>
+          </>
+        ) : (
+          <>
+            <GoodsInfo />
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+                Back
+              </Button>
+ 
+              <Button onClick={handleNext}>{activeStep === steps.length - 1 ? "Finish" : "Next"}</Button>
+            </Box>
+          </>
+        )}
+      </Box>
+    </SimpleBar>
   );
 }
